7d0ca6f211b3d71d47370847c347495867ac7bbb
Max Vanyushkin <max.vanyushkin@gmail.com>
2022-01-18 14:21:49 +0400

    booom

---
+++ main.ts
@@ -0,0 +1,16 @@
+import { Construct } from "constructs";
+import { App, TerraformStack } from "cdktf";
+
+class MyStack extends TerraformStack {
+  constructor(scope: Construct, name: string) {
+    super(scope, name);
+	
+
+
+   
+  }
+}
+
+const app = new App();
+new MyStack(app, "brainfucking_cdktf");
+app.synth();
